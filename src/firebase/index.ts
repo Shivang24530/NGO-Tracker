812a553a9b3637491d014b6efd8d73b383ee0734
Shivang Mittal <mittalshivang8@gmail.com>
2025-11-04 07:27:43 +0000

    Try fixing this error: `Console Error: [2025-11-04T07:26:53.720Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,40 +3,44 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
+
+let firebaseApp: FirebaseApp;
+if (!getApps().length) {
+  try {
+    firebaseApp = initializeApp();
+  } catch (e) {
+    if (process.env.NODE_ENV === 'production') {
+      console.warn(
+        'Automatic initialization failed. Falling back to firebase config object.',
+        e
+      );
+    }
+    firebaseApp = initializeApp(firebaseConfig);
+  }
+} else {
+  firebaseApp = getApp();
+}
+
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+
+export { firebaseApp, auth, firestore };
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
-    return getSdks(firebaseApp);
-  }
-
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
-}
-
-export function getSdks(firebaseApp: FirebaseApp) {
   return {
     firebaseApp,
-    auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    auth,
+    firestore,
+  };
+}
+
+export function getSdks(app: FirebaseApp) {
+  return {
+    firebaseApp: app,
+    auth: getAuth(app),
+    firestore: getFirestore(app),
   };
 }
 
