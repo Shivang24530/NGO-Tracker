43821d50308bba0a0b90f01a4d4ee738e88a4299
Shivang Mittal <mittalshivang8@gmail.com>
2025-11-04 07:01:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8235729385-f4cf4",
+  "appId": "1:592674225283:web:14809db7a2746671216fb6",
+  "apiKey": "AIzaSyAchiObTKX3XiaCUa-LitcCrJ5wzMw7bag",
+  "authDomain": "studio-8235729385-f4cf4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "592674225283"
+};
