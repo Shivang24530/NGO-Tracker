c3e1b3527345d54d990753651290673fb5f4dd94
Shivang Mittal <shivang24530@iiitd.ac.in>
2025-11-19 20:49:56 +0530

    Update firebaseConfig to use placeholder API key
    Replaced the actual API key with a placeholder.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8235729385-f4cf4",
   "appId": "1:592674225283:web:14809db7a2746671216fb6",
-  "apiKey": "AIzaSyAchiObTKX3XiaCUa-LitcCrJ5wzMw7bag",
+  "apiKey": "YOUR API KEY HERE",
   "authDomain": "studio-8235729385-f4cf4.firebaseapp.com",
   "storageBucket": "studio-8235729385-f4cf4.appspot.com",
   "measurementId": "",
